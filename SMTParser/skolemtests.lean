8f823eec631768ae4973fc35e3bfdacfe201ed74
JOSHCLUNE <josh.seth.clune@gmail.com>
2024-05-16 01:45:11 -0400

    Forall support for skolemizeAll

--- SMTParser/skolemtests.lean
+++ SMTParser/skolemtests.lean
@@ -18,7 +18,8 @@ set_option trace.querySMT.debug true
 set_option duper.throwPortfolioErrors true
 set_option querySMT.filterOpt 3
 
-set_option trace.skolemizeAll.debug true
+set_option trace.skolemizeAll.debug false
+set_option pp.proofs true
 
 example (P : Int → Prop) (h : ∃ x : Int, P x) : ∃ y : Int, P y := by
   skolemizeAll
@@ -37,12 +38,139 @@ example (P : Int → Prop) (Q : Int → Prop)
   apply Exists.intro (sk0, sk1)
   exact h1
 
+example (P : Int → Prop) (Q : Int → Prop) (R : Int → Prop)
+  (h : ∃ s : String, ∃ x : Int, ∃ y : Int, ∃ z : Int, P x → Q y ∧ R z) :
+  ∃ a : Int × Int × Int, P a.1 → Q a.2.1 ∧ R a.2.2 := by
+  skolemizeAll
+  apply Exists.intro (sk1, sk2, sk3)
+  exact h
+
 example (x : Int) (h : ∃ y : Int, 2 * y = x) : x ≠ 1 := by
   skolemizeAll
   querySMT
 
+example (x : Int) (h : True ∧ ∃ y : Int, 2 * y = x) : ∃ y : Int, 2 * y = x := by
+  skolemizeAll
+  apply Exists.intro sk0
+  exact h.2
+
+example (h : ∃ x : Int, True ∧ ∃ y : Int, 2 * y = x) : ∃ x : Int, ∃ y : Int, 2 * y = x := by
+  skolemizeAll
+  apply Exists.intro sk0
+  apply Exists.intro sk1
+  exact h.2
+
+example (h : ∃ x : Int, (∃ y : Int, ∃ z : Int, z * y = x) ∧ True) :
+  ∃ x : Int, ∃ y : Int, ∃ z : Int, z * y = x := by
+  skolemizeAll
+  apply Exists.intro sk0
+  apply Exists.intro sk1
+  apply Exists.intro sk2
+  exact h.1
+
+example (h : ∃ x : Int, True ∧ ∃ y : Int, True ∧ ∃ z : Int, z * y = x) :
+  ∃ x : Int, ∃ y : Int, ∃ z : Int, z * y = x := by
+  skolemizeAll
+  apply Exists.intro sk0
+  apply Exists.intro sk1
+  apply Exists.intro sk2
+  exact h.2.2
+
+example (h : ∃ x : Int, (∃ y : Int, x = y) ∧ (∃ z : Int, x ≠ z)) :
+  ∃ x : Int, (∃ y : Int, x = y) ∧ (∃ z : Int, x ≠ z) := by
+  skolemizeAll
+  apply Exists.intro sk0
+  constructor
+  . apply Exists.intro sk1
+    exact h.1
+  . apply Exists.intro sk2
+    exact h.2
+
+set_option pp.proofs true
+
+set_option trace.skolemizeAll.debug true in
+theorem test (h : ∀ x : Int, ∃ y : Int, x = y) :
+  ∀ x : Int, ∃ y : Int, x = y := by
+  skolemizeAll
+  intro x
+  apply Exists.intro (sk0 x)
+  specialize h x
+  exact h
+
+set_option trace.skolemizeAll.debug true in
+example (h : ∀ x : Int, ∀ y : Int, ∃ z : Int, (x ≤ z ∧ z ≤ y) ∨ (y ≤ z ∧ z ≤ x)) :
+  ∀ x : Int, ∀ y : Int, ∃ z : Int, (x ≤ z ∧ z ≤ y) ∨ (y ≤ z ∧ z ≤ x) := by
+  skolemizeAll
+  intros x y
+  specialize h x y
+  apply Exists.intro (sk0 x y)
+  exact h
+
+example (h : ∀ x : Int, (∃ y : Int, y < x) ∧ (∃ y : Int, y > x)) :
+  ∀ x : Int, (∃ y : Int, y < x) ∧ (∃ y : Int, y > x) := by
+  skolemizeAll
+  intro x
+  specialize h x
+  constructor
+  . apply Exists.intro (sk0 x)
+    exact h.1
+  . apply Exists.intro (sk1 x)
+    exact h.2
+
+example (h : ∀ x : Int, (∀ y : Int, ∃ z : Int, z = x + y) ∧ (∀ y : Int, ∃ z : Int, x = z + y)) :
+  ∀ x : Int, (∀ y : Int, ∃ z : Int, z = x + y) ∧ (∀ y : Int, ∃ z : Int, x = z + y) := by
+  skolemizeAll
+  intro x
+  specialize h x
+  constructor
+  . intro y
+    apply Exists.intro (sk0 x y)
+    exact h.1 y
+  . intro y
+    apply Exists.intro (sk1 x y)
+    exact h.2 y
+
+set_option trace.skolemizeAll.debug true in
+theorem test9 (h : ∀ x : Int, ∃ y : Int, x = y) :
+  ∀ x : Int, ∃ y : Int, x = y := by
+  skolemizeAll
+  intro x
+  apply Exists.intro (sk0 x)
+  specialize h x
+  exact h
+
+set_option trace.skolemizeAll.debug true in
+-- set_option pp.all true in
+theorem test1 (h : ∀ x : Int, ∃ y : Int, ∃ z : Int, y < x ∧ x < z) :
+  ∀ x : Int, ∃ y : Int, ∃ z : Int, y < x ∧ x < z := by
+  skolemizeAll
+  -- I'm 99% sure the problem is specifically in the second skolem witness. Look at skolemizeForall dbg output
+  -- In particular, check whether bFVar is being properly abstracted
+  /-
+  skolemWitness after mkLambda (twice): fun _ forallBinder =>
+      (Classical.indefiniteDescription
+          (fun x => (fun forallBinder => Classical.choose _) forallBinder < forallBinder ∧ forallBinder < x)
+          (Classical.choose_spec _)).1
+
+  In the above, we have: `_ : ∃ y z, y < forallBinder✝¹ ∧ forallBinder✝¹ < z`
+  -/
+  intro x
+  apply Exists.intro (sk0 x)
+  apply Exists.intro (sk1 x)
+  exact h x
+
+theorem test2 (h : ∀ x : Int, ∃ y : Int, ∃ z : Int, y < x ∧ x < z) :
+  ∀ x : Int, ∃ y : Int, ∃ z : Int, y < x ∧ x < z := by
+  skolemizeAll
+  intro x
+  apply Exists.intro (x - 1)
+  apply Exists.intro (x + 1)
+  simp
+
+#print test2
+
+/-
 example (x : Int) (h : True ∧ ∃ y : Int, 2 * y = x) : x ≠ 1 := by
-  rcases h with ⟨h1, ⟨h2, h3⟩⟩
   querySMT
   -- This is causing a parse error because of a rewrite involving smtd_0
   -- (original command has smtd_0 in a quanitifer, but the rewrite just refers to smtd_0)
@@ -53,3 +181,4 @@ example (h : ∀ x : Int, ∃ y : Int, y < x) : ∃ z : Int, z < 0 := by
 
 example (x : Int) (h : False ∨ ∃ y : Int, 2 * y = x) : x ≠ 1 := by
   querySMT
+-/
--- SMTParser/tests.lean
+++ SMTParser/tests.lean
@@ -4,6 +4,7 @@ set_option auto.smt true
 set_option auto.smt.trust true
 set_option auto.smt.solver.name "cvc5"
 set_option auto.smt.dumpHints true
+set_option auto.smt.dumpHints.limitedRws true
 
 set_option auto.smt.save false
 set_option auto.smt.savepath "/Users/joshClune/Desktop/temp.smt"
@@ -20,6 +21,7 @@ set_option querySMT.filterOpt 3
 example (x y z : Int) : x ≤ y → y ≤ z → x ≤ z := by
   querySMT
 
+set_option auto.smt.dumpHints.limitedRws false in
 example (x y z : Int) : x < y → y < z → x < z := by
   querySMT
 
@@ -30,41 +32,21 @@ example {a b c d e f : Int} (h : a * b = c * d) (h' : e = f) : a * (b * e) = c *
   querySMT -- `proveSMTLemma` is insufficient to prove the theory lemma returned by cvc5
 
 example : True → ∀ x : Int, ∀ y : Int, ∀ z : Int, x ≤ y → y ≤ z → x ≤ z := by
-  intros _ x y z
-  have smtLemma0 : ¬x ≤ z → x + -Int.ofNat 1 * z ≥ Int.ofNat 1 := by proveSMTLemma
-  have smtLemma1 : y ≤ z → ¬y + -Int.ofNat 1 * z ≥ Int.ofNat 1 := by proveSMTLemma
-  have smtLemma2 : x ≤ y → ¬x + -Int.ofNat 1 * y ≥ Int.ofNat 1 := by proveSMTLemma
-  have smtLemma3 :
-    (x + -Int.ofNat 1 * y ≥ Int.ofNat 1 ∨ y + -Int.ofNat 1 * z ≥ Int.ofNat 1) ∨ ¬x + -Int.ofNat 1 * z ≥ Int.ofNat 1 :=
-    by proveSMTLemma
-  duper [*]
+  querySMT
 
 example : ∀ x : Int × Int × Int, x.1 ≤ x.2.1 → x.2.1 ≤ x.2.2 → x.1 ≤ x.2.2 := by
-  intros x
-  have smtLemma0 : ¬x.1 ≤ x.2.2 → x.1 + -Int.ofNat 1 * x.2.2 ≥ Int.ofNat 1 := by proveSMTLemma
-  have smtLemma1 : x.2.1 ≤ x.2.2 → ¬x.2.1 + -Int.ofNat 1 * x.2.2 ≥ Int.ofNat 1 := by proveSMTLemma
-  have smtLemma2 : x.1 ≤ x.2.1 → ¬x.1 + -Int.ofNat 1 * x.2.1 ≥ Int.ofNat 1 := by proveSMTLemma
-  have smtLemma3 :
-    (x.1 + -Int.ofNat 1 * x.2.1 ≥ Int.ofNat 1 ∨ x.2.1 + -Int.ofNat 1 * x.2.2 ≥ Int.ofNat 1) ∨
-      ¬x.1 + -Int.ofNat 1 * x.2.2 ≥ Int.ofNat 1 :=
-    by proveSMTLemma
-  duper [*]
-
-example (x y z : Nat) : x < y → y < z → x < z := by
-  querySMT -- TODO: Look into incorporating `zify` in the preprocessing (or a better version of it)
+  querySMT
 
 example (x : Int) : ∃ y : Int, y < x := by
   querySMT -- `proveSMTLemma` is insufficient to prove the lemma returned by cvc5 (basically just double-negated goal)
 
 example (x : Int) (h : ∃ y : Int, 2 * y = x) : x ≠ 1 := by
-  querySMT -- Fails because of skolemization issue
+  skolemizeAll
+  querySMT
 
 example (x : Int) (h : True ∧ ∃ y : Int, 2 * y = x) : x ≠ 1 := by
-  querySMT -- Fails because of skolemization issue
-  -- This example is one in which lean-auto doesn't skolemize but cvc5 does. So I need
-  -- to be pretty general with my skolemization
-  -- cvc5 will skolemize "top-level" existential quantifiers, though based on this
-  -- example, that's presumably "top-level" after some preprocessing/normalization
+  skolemizeAll
+  querySMT
 
 example (x : Int) (h : False ∨ ∃ y : Int, 2 * y = x) : x ≠ 1 := by
   querySMT
@@ -73,10 +55,19 @@ example (x y : Int) (h : x * y ≠ 0) : x ≠ 0 ∧ y ≠ 0 := by
   querySMT
 
 example : ∀ x : Int, ∀ y : Int, x * y ≠ 0 → x ≠ 0 ∧ y ≠ 0 := by
-  intros x y
-  have smtLemma0 : y = Int.ofNat 0 → x * y = Int.ofNat 0 := by proveSMTLemma
-  have smtLemma1 : x = Int.ofNat 0 → x * y = Int.ofNat 0 := by proveSMTLemma
-  duper [*]
+  querySMT
+
+example (P : Int × Int → Prop) (h : ∀ x : Int, ∀ y : Int, P (x, y)) :
+  ∃ z : Int × Int, P z := by
+  querySMT
+
+example (P : Int × Int → Prop) (h : ∀ x : Int, ∀ y : Int, P (x, y)) :
+  ∀ x : Int, ∀ y : Int, P (x, y) := by
+  querySMT -- varName: smti_0 maps to an atom other than term
+
+example (P : Int × Int → Prop) (h : ∀ x : Int, ∀ y : Int, P (x, y)) :
+  ∀ z : Int × Int, P z := by
+  querySMT
 
 -- set_option trace.duper.printProof true in
 example (Even Odd : Int → Prop)
@@ -86,5 +77,77 @@ example (Even Odd : Int → Prop)
   (h4 : Odd (1)) : Even (10) := by
   querySMT
 
+example (Pos Neg Zero : Int → Prop)
+  -- (h0 : ∀ x : Int, Pos x → ¬ Zero x)
+  -- (h1 : ∀ x : Int, Neg x → ¬ Zero x)
+  -- (h2 : ∀ x : Int, Zero x → (¬ Pos x) ∧ (¬ Neg x))
+  -- (h3 : ∀ x : Int, Pos x ∨ Neg x ∨ Zero x)
+  (h4 : ∀ x : Int, Pos x → Pos (x + 1))
+  (h5 : Pos 1)
+  -- (h6 : Zero 0)
+  /- (h7 : ∀ x : Int, Pos x ↔ Neg (- x)) -/ : Pos 2 := by
+  querySMT
+
+example (Pos Neg Zero : Int → Prop)
+  -- (h0 : ∀ x : Int, Pos x → ¬ Zero x)
+  -- (h1 : ∀ x : Int, Neg x → ¬ Zero x)
+  -- (h2 : ∀ x : Int, Zero x → (¬ Pos x) ∧ (¬ Neg x))
+  -- (h3 : ∀ x : Int, Pos x ∨ Neg x ∨ Zero x)
+  (h4 : ∀ x : Int, Pos x → Pos (x + 1))
+  (h5 : Pos 1)
+  -- (h6 : Zero 0)
+  (h7 : ∀ x : Int, Pos x ↔ Neg (- x)) : Pos 2 := by
+  querySMT
+
+example (Pos Neg Zero : Int → Prop)
+  -- (h0 : ∀ x : Int, Pos x → ¬ Zero x)
+  -- (h1 : ∀ x : Int, Neg x → ¬ Zero x)
+  -- (h2 : ∀ x : Int, Zero x → (¬ Pos x) ∧ (¬ Neg x))
+  -- (h3 : ∀ x : Int, Pos x ∨ Neg x ∨ Zero x)
+  (h4 : ∀ x : Int, Neg x → Neg (x - 1))
+  (h5 : Neg (-1))
+  -- (h6 : Zero 0)
+  /- (h7 : ∀ x : Int, Pos x ↔ Neg (- x)) -/ : Neg (-2) := by
+  querySMT
+
+-- set_option auto.smt.save true in
+example (Pos Neg Zero : Int → Prop)
+  -- (h0 : ∀ x : Int, Pos x → ¬ Zero x)
+  -- (h1 : ∀ x : Int, Neg x → ¬ Zero x)
+  -- (h2 : ∀ x : Int, Zero x → (¬ Pos x) ∧ (¬ Neg x))
+  -- (h3 : ∀ x : Int, Pos x ∨ Neg x ∨ Zero x)
+  (h4 : ∀ x : Int, Neg x → Neg (x - 1))
+  (h5 : Neg (-1))
+  -- (h6 : Zero 0)
+  (h7 : ∀ x : Int, Pos x ↔ Neg (- x)) : Neg (-2) := by
+  querySMT
+
+example (Pos Neg Zero : Int → Prop)
+  -- (h0 : ∀ x : Int, Pos x → ¬ Zero x)
+  -- (h1 : ∀ x : Int, Neg x → ¬ Zero x)
+  -- (h2 : ∀ x : Int, Zero x → (¬ Pos x) ∧ (¬ Neg x))
+  -- (h3 : ∀ x : Int, Pos x ∨ Neg x ∨ Zero x)
+  (h4 : ∀ x : Int, Neg (- x) → Neg (-(x + 1)))
+  (h5 : Neg (-1))
+  -- (h6 : Zero 0)
+  /- (h7 : ∀ x : Int, Pos x ↔ Neg (- x)) -/ : Neg (- 2) := by
+  querySMT
+
+-- set_option auto.smt.save true in
+example (Pos Neg Zero : Int → Prop)
+  (h4 : ∀ x : Int, Neg (- x) → Neg ((- x) - 1))
+  (h5 : Neg (-1)) : Neg (- 2) := by
+  querySMT
+
+/-
 example (x y : Real) : x < y ∨ y ≤ x := by
   querySMT -- Fails because lean-auto doesn't depend on Mathlib and therefore doesn't know about Reals
+
+example (x y z : Nat) : x < y → y < z → x < z := by
+  querySMT -- TODO: Look into incorporating `zify` in the preprocessing (or a better version of it)
+-/
+
+theorem forallExists : ∀ x : Int, ∃ y : Int, x ≤ y := by
+  intros x
+  have smtLemma0 : (¬∃ smtd_0, x ≤ smtd_0) → False := by proveSMTLemma
+  duper [*]
