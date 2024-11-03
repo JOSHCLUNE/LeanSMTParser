48757019d1d2680d4764aba59dfd5e020c450fe8
JOSHCLUNE <josh.seth.clune@gmail.com>
2024-11-03 18:29:27 -0500

    Change duper dependency to specific commit

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.12.0"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "dev"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "a500ea7a5b9eca0ecaa7b4a229786a61b2707d30"
 
 package QuerySMT {
   precompileModules := true
