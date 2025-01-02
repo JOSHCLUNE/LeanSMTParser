12febbf63699fd1b26d7d47d7d3eadce6727f1ef
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-01-01 20:49:12 -0500

    Update lean-auto

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.14.0"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "dd5c32ec8bd822bc4b80425325a60bbb8a4e8626"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "707dc8fa41f68062efd90fd6aae24a93681bf8f6"
 
 package QuerySMT {
   precompileModules := true
