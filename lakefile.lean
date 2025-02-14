33d1a8916fe0ce219aa19a9167812f430fe3c511
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-02-14 14:34:42 -0500

    Update Duper version

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,9 @@
 import Lake
 open Lake DSL
 
-require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.15.0"
+require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.15.0-patch1"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "3303ab6fd8e454ca5ffa92cae2d7001d54bd86cd"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "fde2ebf85f572a6b4e151ef9b3b35dbebafc7621"
 
 package QuerySMT {
   precompileModules := true
