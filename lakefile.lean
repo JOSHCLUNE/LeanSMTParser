dd3bbf28d2b593355f40346b72d82e82bacd001b
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-03-02 00:01:42 -0500

    Switch toolchain to v4.15.0

--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,11 @@
 import Lake
 open Lake DSL
 
-require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.16.0"
+require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.15.0-patch1"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "v0.0.23"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "efd479f7da1a29aa7a77aaead0198e7947830a7a"
 
-require «premise-selection» from git "https://github.com/hanwenzhu/premise-selection" @ "v4.16.0"
+require «premise-selection» from git "https://github.com/hanwenzhu/premise-selection" @ "v4.15.0"
 
 package QuerySMT {
   precompileModules := true
