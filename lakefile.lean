842ca2a4a780f1a3589c930ef82ebb08368682f7
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-03-03 20:25:08 -0500

    lean-auto update that improves <=> TPTP translation

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.16.0"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "v0.0.23"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "a4b311892a75afdd2e4a58de7422d6a1ad9655d1"
 
 require «premise-selection» from git "https://github.com/hanwenzhu/premise-selection" @ "v4.16.0"
 
